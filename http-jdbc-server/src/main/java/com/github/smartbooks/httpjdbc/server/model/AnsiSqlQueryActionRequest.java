2d1038bf6ba2abd07b33e9b999cfb526a9937955
smartbooks <smartbooks@qq.com>
2018-03-23 16:09:16 +0800

    666

--- http-jdbc-server/src/main/java/com/github/smartbooks/httpjdbc/server/model/AnsiSqlQueryActionRequest.java
+++ http-jdbc-server/src/main/java/com/github/smartbooks/httpjdbc/server/model/AnsiSqlQueryActionRequest.java
@@ -1,49 +1,40 @@
 package com.github.smartbooks.httpjdbc.server.model;
 
-public class AnsiSqlQueryActionRequest
-{
+public class AnsiSqlQueryActionRequest {
     private String sql;
     private Long ttl;
     private String alias;
     private String token;
 
-    public String getSql()
-    {
+    public String getSql() {
         return sql;
     }
 
-    public void setSql(String sql)
-    {
+    public void setSql(String sql) {
         this.sql = sql;
     }
 
-    public Long getTtl()
-    {
+    public Long getTtl() {
         return ttl;
     }
 
-    public void setTtl(Long ttl)
-    {
+    public void setTtl(Long ttl) {
         this.ttl = ttl;
     }
 
-    public String getAlias()
-    {
+    public String getAlias() {
         return alias;
     }
 
-    public void setAlias(String alias)
-    {
+    public void setAlias(String alias) {
         this.alias = alias;
     }
 
-    public String getToken()
-    {
+    public String getToken() {
         return token;
     }
 
-    public void setToken(String token)
-    {
+    public void setToken(String token) {
         this.token = token;
     }
 }
